d32c751254cd039aff5d5dcb2a93b8cb9193d1c8
Furkan Aydin <furkan.aydin@mail.schwarz>
2024-08-24 17:31:11 +0200

    feature:admin and user dashboard

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -11,12 +11,25 @@ const bookList = document.getElementById('books');
 const bookForm = document.getElementById('bookForm');
 const addEditBookForm = document.getElementById('addEditBookForm');
 
+const displayUsername = document.getElementById('displayUsername');
+
+const optionForm = document.getElementById('optionForm');
+const editBtn = document.getElementById('editBtn');
+const readBtn = document.getElementById('readBtn');
+const orderListBtn = document.getElementById('orderListBtn');
+const booksRequestBtn = document.getElementById('booksRequestBtn');
+const editRequestBtn = document.getElementById('editRequestBtn');
+
+const logoutOptions = { redirectUri: "http://127.0.0.1:5500/index.html" }
+
 loginBtn.onclick= ()=> keycloak.login();
-logoutBtn.onclick= ()=> keycloak.logout();
+logoutBtn.onclick= ()=> keycloak.logout(logoutOptions);
+registerBtn.onclick= ()=> keycloak.register();
 
 const updateButtons = (authenticated)=> {
     loginBtn.style.display = authenticated ? 'none' : 'inline-block';
     logoutBtn.style.display = authenticated ? 'inline-block' : 'none';
+    registerBtn.style.display = authenticated ? 'none' : 'inline-block';
 }
 
 const keycloak = new Keycloak({
@@ -33,16 +46,47 @@ window.onload=function() {
         checkLoginIframe: false
     }).then(function (authenticated) {
         console.log(authenticated ? 'Authenticated' : 'Not authenticated');
-        // updateButtons(authenticated)
+        jwtToken = keycloak.token;
+        //console.log('JWT Token:' + jwtToken);
+        updateButtons(authenticated);
         if(authenticated) {
             fetchBooks();
+            showDashboard();
         } 
     }).catch(function(error) {
         console.log('Failed to initilaice keycloak', error);
     })
 }
 
+function showDashboard() {
+    if (keycloak.tokenParsed) {
+        const userName = keycloak.tokenParsed.preferred_username;
+        const roles = keycloak.realmAccess.roles;
 
+        optionForm.style.display = 'inline-block';
+
+        console.log('Logged in user: ', userName);
+        console.log('User roles: ', roles);
+
+        if (roles.includes('bm_admin')) {
+            showAdminDashBoard(userName);
+        } else if (roles.includes('bm_user')) {
+            showUserDashboard(userName);
+        } else {
+            console.log('Hello World!');
+        }
+    }
+}
+
+function showAdminDashBoard(userName) {
+    displayUsername.innerHTML = `Welcome, Admin ${userName}`;
+    optionsForAdmin();
+}
+
+function showUserDashboard(userName) {
+    displayUsername.innerHTML = `Welcome, ${userName}`;
+    optionsForUser();
+}
 
 setInterval(()=> {
     keycloak.updateToken(70).then((refreshed)=> {
@@ -57,24 +101,36 @@ setInterval(()=> {
 }, 60000);
 
 
-function updateUIForLoggedInUser() {
-    loginBtn.style.display = 'none';
-    registerBtn.style.display = 'none';
-    logoutBtn.style.display = 'inline';
-    bookForm.style.display = 'block';
+function optionsForAdmin() {
+    editBtn.style.display = 'inline';
+    readBtn.style.display = 'inline';
+    orderListBtn.style.display = 'inline';
+    booksRequestBtn.style.display = 'inline';
+    editRequestBtn.style.display = 'inline';
+    bookForm.style.display = 'none';
+
 }
 
-function updateUIForLoggedOutUser() {
-    loginBtn.style.display = 'inline';
-    registerBtn.style.display = 'inline';
-    logoutBtn.style.display = 'none';
-    bookForm.style.display = 'none';
+editBtn.onclick = () => {
+    if (bookForm.style.display === 'none' || bookForm.style.display === '') {
+        bookForm.style.display = 'block';
+    } else {
+        bookForm.style.display = 'none';
+    }
+}
+
+function optionsForUser() {
+    editBtn.style.display = 'none';
+    readBtn.style.display = 'inline';
+    orderListBtn.style.display = 'none';
+    booksRequestBtn.style.display = 'inline';
+    editRequestBtn.style.display = 'none';
     bookList.innerHTML = '';
 }
 
 async function fetchBooks() {
     try {
-        const response = await fetch('/api/books', {
+        const response = await fetch('http://localhost:8080/api/books', {
             headers: {
                 'Authorization': `Bearer ${jwtToken}`
             }
@@ -83,6 +139,7 @@ async function fetchBooks() {
         renderBooks();
     } catch (error) {
         console.error('Fehler beim Abrufen der Bücher:', error);
+        console.log(books);
     }
 }
 
@@ -124,7 +181,7 @@ async function handleBookSubmit(e) {
 }
 
 async function createBook(bookData) {
-    await fetch('/api/books', {
+    await fetch('http://localhost:8080/api/books', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -135,7 +192,7 @@ async function createBook(bookData) {
 }
 
 async function updateBook(id, bookData) {
-    await fetch(`/api/books/${id}`, {
+    await fetch(`http://localhost:8080/api/books/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -158,7 +215,7 @@ function editBook(id) {
 async function deleteBook(id) {
     if (confirm('Sind Sie sicher, dass Sie dieses Buch löschen möchten?')) {
         try {
-            await fetch(`/api/books/${id}`, {
+            await fetch(`http://localhost:8080/api/books/${id}`, {
                 method: 'DELETE',
                 headers: {
                     'Authorization': `Bearer ${jwtToken}`
